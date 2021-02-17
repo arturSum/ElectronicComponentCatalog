7245dc77d2ed0fa7930eec70e18ae168f8da7fc9
Sum Artur <sum.artur@gmail.com>
2021-02-17 20:51:22 +0100

    add main class to project

---
+++ src/main/java/com/arturSum/ElectronicComponentCatalog/ElectronicComponentCatalog.java
@@ -0,0 +1,12 @@
+package com.arturSum.ElectronicComponentCatalog;
+
+public class ElectronicComponentCatalog {
+
+
+    public static void main(String[] args){
+
+        System.out.println("Hello: ElectronicComponentCatalog");
+
+    }
+
+}
